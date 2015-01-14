f889844e14def0770b02df81625bbcac65dbe9b7
wyrllyn <sara.tari101010@gmail.com>
2015-01-14 14:41:37 +0100

    first addition version

--- BigInteger.cu
+++ BigInteger.cu
@@ -95,3 +95,97 @@ int main(int argc, char** argv) {
 	}
 
 }
+
+
+
+//init 
+// size = vali + hjvo +1 (addition case)
+__device__ void init(int size, char* toFill) {
+	for (int i = 0; i < size; i++) {
+		toFill[i] = 0;
+	}
+}
+
+//return size, update char*
+__device__ int update(char* toUpdate, int value) {
+	int dec = 0;
+	for (int i = 0; i < value; i++) {
+		if (toUpdate[i] == 0) {
+			dec++;
+		}
+		else {
+			break;
+		}
+	}
+
+	int toReturn = value-dec;
+
+	for (int i = 0; i < toReturn; i++) {
+		toUpdate[i] = toUpdate[i+dec];
+	}
+
+
+	return toReturn;
+}
+
+/*
+How to use this method :
+- newB and size newB contains the result
+- vali is the biggest size
+- hjvo is the difference between sizes
+- first is the biggest char*
+- second is the other
+- calcul newB avant
+
+*/
+
+__global__ void kernel_add(char* newB, char* first, char* second, int vali, int hjvo, int * size_newB) {
+	int tmp = 0;
+	int carry = 0;
+	(*size_newB) = vali + hjvo +1;
+	init(*size_newB, newB);
+	int index = *size_newB;
+
+/*
+	// know 
+	if (size_first > size_second) {
+		vali = size_second;
+		hjvo = size_first - size_second;
+	}
+	else {
+		vali = size_first;
+		hjvo = size_second - size_first;
+	}
+
+*/
+
+	for (int i = vali - 1; i >= 0; i--) {
+		tmp = second[i] + first[i] + carry;
+		if (tmp >= 10) {
+			carry = 1;
+			tmp = tmp % 10;
+		}
+		else {
+			carry = 0;
+		}
+		newB[index] = tmp;
+		index--;
+	}
+
+	for (int i = hjvo - 1; i >= 0; i--) {
+		tmp = first[i] + carry;
+		if (tmp >= 10) {
+			carry = 1;
+			tmp = tmp % 10;
+		}
+		else {
+			carry = 0;
+		}
+		newB[index] = tmp;
+		index--;
+	}
+	if (carry != 0) {
+		newB[index] = carry;
+	}
+
+}
