d5ecad59603d0b4418c3e863ec2b5ae3fa3ebff6
X3N0-Life-Form <adrarsis@laposte.net>
2015-01-14 16:30:05 +0100

    rigged add as a cpu function & made it work also added several prints for debugging purposes + renammed a couple of variables + moved the main() back to the bottom of the page

--- BigInteger.cu
+++ BigInteger.cu
@@ -45,69 +45,17 @@ void BigInteger::zero() {
 	}
 }
 
-
-int main(int argc, char** argv) {
-
-	BigInteger left, right;
-	OperationType opType;
-	if (argc >= 2) {
-		opType = identifyOperationType(argv[1]);
-		left.setNumber(argv[2], string(argv[2]).size());
-		left.zero();
-		switch (opType) {
-		case ADD:
-		case SUBSTRACT:
-		case MULTIPLY:
-		case DIVIDE:
-			right.setNumber(argv[3], string(argv[3]).size());
-			right.zero();
-			break;
-		case ERROR:
-			cout << "Unrecognised operation type: " << argv[1] << endl;
-		}
-	} else {
-		cout << "Insufficient number of arguments" << endl;
+void bump(char* number, int size) {
+	for (int i = 0; i < size; i++) {
+		number[i] += '0';
 	}
-
-	BigInteger* g_left, *g_right;
-	cudaMalloc( (void**) &g_left, sizeof(BigInteger));
-	cudaMalloc( (void**) &g_right, sizeof(BigInteger));
-
-	cudaMemcpy(g_left, &left, sizeof(BigInteger), cudaMemcpyHostToDevice);
-	cudaMemcpy(g_right, &right, sizeof(BigInteger), cudaMemcpyHostToDevice);
-
-	dim3 block, grid;
-	switch (opType) {
-	case ADD:
-
-		break;
-	case SUBSTRACT:
-
-		break;
-	case MULTIPLY:
-
-		break;
-	case DIVIDE:
-
-		break;
-	case FACTORIAL:
-
-		break;
-
-	case GCD:
-
-		break;
-	default:
-		cout << "Reaching default case." << endl;
-		break;
-	}
-
 }
 
 
 
+
 //init 
-// size = vali + hjvo +1 (addition case)
+// size = size_biggest + diff +1 (addition case)
 __device__ void init(int size, char* toFill) {
 	for (int i = 0; i < size; i++) {
 		toFill[i] = 0;
@@ -139,36 +87,37 @@ __device__ int update(char* toUpdate, int value) {
 /*
 How to use this method :
 - newB and size newB contains the result
-- vali is the biggest size
-- hjvo is the difference between sizes
+- size_biggest is the biggest size
+- diff is the difference between sizes
 - first is the biggest char*
 - second is the other
 - calcul newB avant
 
 */
 
-__global__ void kernel_add(char* newB, char* first, char* second, int vali, int hjvo, int * size_newB) {
+/*__global__*/ void kernel_add(char* newB, char* first, char* second, int size_biggest, int diff, int * size_newB) {
 	int tmp = 0;
 	int carry = 0;
-	(*size_newB) = vali + hjvo +1;
+	(*size_newB) = size_biggest + 1;
 	init(*size_newB, newB);
-	int index = *size_newB;
+	int index = *size_newB - 1;
 
 /*
 	// know 
 	if (size_first > size_second) {
-		vali = size_second;
-		hjvo = size_first - size_second;
+		size_biggest = size_second;
+		diff = size_first - size_second;
 	}
 	else {
-		vali = size_first;
-		hjvo = size_second - size_first;
+		size_biggest = size_first;
+		diff = size_second - size_first;
 	}
 
 */
 
-	for (int i = vali - 1; i >= 0; i--) {
+	for (int i = size_biggest - 1; i >= 0; i--) {
 		tmp = second[i] + first[i] + carry;
+cout << "__ " << (int) first[i] << " + " << (int) second[i] << " + " << carry << " = " << tmp << endl;
 		if (tmp >= 10) {
 			carry = 1;
 			tmp = tmp % 10;
@@ -177,11 +126,13 @@ __global__ void kernel_add(char* newB, char* first, char* second, int vali, int 
 			carry = 0;
 		}
 		newB[index] = tmp;
+cout << index << "___ " << (int) newB[index] << endl;
 		index--;
 	}
 
-	for (int i = hjvo - 1; i >= 0; i--) {
+	/*for (int i = diff - 1; i >= 0; i--) {
 		tmp = first[i] + carry;
+cout << "~~ " << (int) first[i] << " + " << carry << " = " << tmp << endl;
 		if (tmp >= 10) {
 			carry = 1;
 			tmp = tmp % 10;
@@ -190,10 +141,104 @@ __global__ void kernel_add(char* newB, char* first, char* second, int vali, int 
 			carry = 0;
 		}
 		newB[index] = tmp;
+cout << index << "~~~ " << (int) newB[index] << endl;
 		index--;
-	}
+	}*/
 	if (carry != 0) {
 		newB[index] = carry;
+cout << index << "### " << (int) newB[index] << endl;
+	}
+
+cout << "cheking final result" << endl;
+for (int i = 0; i < *size_newB; i++) {
+	cout << (int) newB[i];
+}
+cout << endl;
+
+}
+
+int main(int argc, char** argv) {
+
+	BigInteger left, right;
+	OperationType opType;
+	if (argc >= 2) {
+		opType = identifyOperationType(argv[1]);
+		left.setNumber(argv[2], string(argv[2]).size());
+		left.zero();
+		switch (opType) {
+		case ADD:
+		case SUBSTRACT:
+		case MULTIPLY:
+		case DIVIDE:
+			right.setNumber(argv[3], string(argv[3]).size());
+			right.zero();
+			break;
+		case ERROR:
+			cout << "Unrecognised operation type: " << argv[1] << endl;
+		}
+	} else {
+		cout << "Insufficient number of arguments" << endl;
+	}
+
+	BigInteger* g_left, *g_right;
+	cudaMalloc( (void**) &g_left, sizeof(BigInteger));
+	cudaMalloc( (void**) &g_right, sizeof(BigInteger));
+
+	cudaMemcpy(g_left, &left, sizeof(BigInteger), cudaMemcpyHostToDevice);
+	cudaMemcpy(g_right, &right, sizeof(BigInteger), cudaMemcpyHostToDevice);
+
+	dim3 block(1), grid(1);
+	///
+	/// Testing block
+	///
+	char* nu = new char[2], * g_nu;
+	char* first = new char[2];
+	first[0] = 9; first[1] = 2;
+	char* second = new char[2];
+	second[0] = 1; second[1] = 9;
+	int nuSize = 2;
+	/*
+	char* g_first, *g_second;
+	cudaMalloc( (void**) &g_first, sizeof(char) * 2 );
+	cudaMalloc( (void**) &g_second, sizeof(char) * 2 );
+	cudaMalloc( (void**) &g_nu, sizeof(char) * 2 );
+	cudaMemcpy(g_first, first, sizeof(char) * 2, cudaMemcpyHostToDevice);
+	cudaMemcpy(g_second, second, sizeof(char) * 2, cudaMemcpyHostToDevice);
+	kernel_add<<<grid, block>>>(g_nu, g_first, g_second, 2, 2, &nuSize);
+	cudaMemcpy(nu, g_nu, sizeof(char) * 2, cudaMemcpyDeviceToHost);
+	*/
+kernel_add(nu, first, second, 2, 2, &nuSize);
+	cout << (int) first[0] << (int) first[1] << " + " << (int) second[0] << (int) second[1] << " = " << (int) nu[0] << (int) nu[1] << (int) nu[2]<< endl;
+	///
+	/// End of testing block
+	///
+	switch (opType) {
+	case ADD:
+
+		break;
+	case SUBSTRACT:
+
+		break;
+	case MULTIPLY:
+
+		break;
+	case DIVIDE:
+
+		break;
+	case FACTORIAL:
+
+		break;
+
+	case GCD:
+
+		break;
+	default:
+		cout << "Reaching default case." << endl;
+		break;
 	}
 
 }
+
+///
+/// Reminder: Don't put functions or methods below the main
+///
--- BigInteger.h
+++ BigInteger.h
@@ -35,10 +35,10 @@ public:
 	__device__ __host__ void greatestCommonDivisor(const BigInteger& other);
 };
 
-__device__ int update(char* toUpdate, int value);
-__device__ void init(int size, char* toFill);
+__device__ __host__ int update(char* toUpdate, int value);
+__device__ __host__ void init(int size, char* toFill);
 
-__global__ void kernel_add(char* newB, const char* first, const char* second, int size_first, int size_second, int * size_newB);
+/*__global__*/ void kernel_add(char* newB, char* first, char* second, int size_first, int size_second, int * size_newB);
 __global__ void kernel_sub(char* newB, const char* first, const char* second, int size_first, int size_second, int * size_newB);
 __global__ void kernel_mul(char* newB, const char* first, const char* second, int size_first, int size_second, int * size_newB);
 __global__ void kernel_div(char* newB, const char* first, const char* second, int size_first, int size_second, int * size_newB);
