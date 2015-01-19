af8d39bd52bac50f138a300d661789f8a78cbe9e
X3N0-Life-Form <adrarsis@laposte.net>
2015-01-19 15:16:33 +0100

    time tracking + minor neg management + commented out testing section + added a dislcaimer at the beginning

--- BigInteger.cu
+++ BigInteger.cu
@@ -41,6 +41,7 @@ void BigInteger::setNumber(const char* nuNumber, int nuSize) {
 
 void BigInteger::zero() {
 	for (int i = 0; i < size; i++) {
+		if (number[i] == '-') continue;
 		number[i] -= '0';
 	}
 }
@@ -381,6 +382,8 @@ void kernel_div(char* newB, const char* first, const char* second, int size_firs
 }
 
 int main(int argc, char** argv) {
+	cout << "This program assumes that its user enters coherent arguments, such as -25 + 12" << endl;
+	cout << "Enter boggus data at your own risks..." << endl;
 
 	BigInteger left, right;
 	OperationType opType;
@@ -403,6 +406,11 @@ int main(int argc, char** argv) {
 		cout << "Insufficient number of arguments" << endl;
 	}
 
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start, 0);
+
 	switch (opType) {
 	case ADD:
 		left.add(right);
@@ -422,11 +430,21 @@ int main(int argc, char** argv) {
 	case GCD:
 		left.greatestCommonDivisor(right);
 		break;
+	default:
+		cout << "Reaching default case." << endl;
+		break;
 	}
+
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	float elapsed_time;
+	cudaEventElapsedTime(&elapsed_time, start, stop);
+	cout << "time: " << elapsed_time << " ms" << endl;
+
 	///
 	/// Testing block
 	///
-	#define SIZE_FIRST 2
+	/*#define SIZE_FIRST 2
 	#define SIZE_SECOND 2
 	#define NU_SIZE 4
 	char* nu = new char[NU_SIZE], * g_nu;
@@ -434,7 +452,7 @@ int main(int argc, char** argv) {
 	first[0] = 3; first[1] = 5;
 	char* second = new char[SIZE_SECOND];
 	second[0] = 2; second[1] = 0; // second[2] = 2;
-	int nuSize = NU_SIZE;
+	int nuSize = NU_SIZE;*/
 	/*
 	char* g_first, *g_second;
 	cudaMalloc( (void**) &g_first, sizeof(char) * 2 );
@@ -447,8 +465,8 @@ int main(int argc, char** argv) {
 	*/
 	//kernel_add(nu, first, second, SIZE_FIRST, SIZE_FIRST - SIZE_SECOND, &nuSize);
 	//kernel_div(nu, first, second, SIZE_FIRST, SIZE_FIRST - SIZE_SECOND, &nuSize);
-	kernel_mul(nu, first, second, SIZE_FIRST, /*SIZE_FIRST -*/ SIZE_SECOND, &nuSize);
-	for (int i = 0; i < SIZE_FIRST; i++) {
+	//kernel_mul(nu, first, second, SIZE_FIRST, /*SIZE_FIRST -*/ SIZE_SECOND, &nuSize);
+	/*for (int i = 0; i < SIZE_FIRST; i++) {
 		cout << (int) first[i];
 	}
 	cout << " * ";
@@ -459,35 +477,10 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < nuSize; i++) {
 		cout << (int) nu[i];
 	}
-	cout << endl;
+	cout << endl;*/
 	///
 	/// End of testing block
 	///
-	switch (opType) {
-	case ADD:
-
-		break;
-	case SUBSTRACT:
-
-		break;
-	case MULTIPLY:
-
-		break;
-	case DIVIDE:
-
-		break;
-	case FACTORIAL:
-
-		break;
-
-	case GCD:
-
-		break;
-	default:
-		cout << "Reaching default case." << endl;
-		break;
-	}
-
 }
 
 ///
