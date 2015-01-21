f2bc476929486ba1eb007e6bc78472e8776f3c78
X3N0-Life-Form <adrarsis@laposte.net>
2015-01-21 14:24:31 +0100

    refactoring: moved kernel functions to their own file

--- BigInteger.h
+++ BigInteger.h
@@ -1,6 +1,9 @@
 #ifndef BIGINTEGER_H
 #define BIGINTEGER_H
 
+
+#include "kernel.h"
+
 enum OperationType {
 	ADD,
 	SUBSTRACT,
@@ -36,13 +39,5 @@ public:
 	__device__ __host__ void greatestCommonDivisor(const BigInteger& other);
 };
 
-
-/*__global__*/ void kernel_add(char* newB, char* first, char* second, int size_first, int size_second, int * size_newB);
-/*__global__ */void kernel_sub(char* newB, const char* first, const char* second, int size_biggest, int diff, int * size_newB);
-/*__global__*/ void kernel_mul(char* newB, const char* first, const char* second, int size_first, int size_second, int * size_newB);
-/*__global__*/ void kernel_div(char* newB, const char* first, const char* second, int size_first, int size_second, int * size_newB);
-
-__global__ void kernel_fact(char* newB, const char* first, int size_first, int * size_newB);
-__global__ void kernel_GCD(char* newB, const char* first, int size_first, int * size_newB);
 
 #endif
--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 all : gpu.exe
 
 
-gpu.exe: main.cu BigInteger.o utility.o
-	nvcc  $< -o $@ --compiler-options -O2 BigInteger.o utility.o
+gpu.exe: main.cu BigInteger.o utility.o kernel.o
+	nvcc  $< -o $@ --compiler-options -O2 BigInteger.o utility.o kernel.o
 
 BigInteger.o: BigInteger.cu
 	nvcc  $< -c --compiler-options -O2
 
 utility.o: utility.cu
+	nvcc $< -c --compiler-options -O2
+
+kernel.o: kernel.cu
 	nvcc $< -c --compiler-options -O2
 
 clean:
