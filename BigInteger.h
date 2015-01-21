066885c4129d46d142cb086e308eeaa88cc407a2
X3N0-Life-Form <adrarsis@laposte.net>
2015-01-21 14:17:02 +0100

    further refactoring: utility functions have been moved to utility.cu

--- BigInteger.h
+++ BigInteger.h
@@ -37,12 +37,6 @@ public:
 };
 
 
-bool isNeg(char* bi);
-__device__ __host__ int update(char* toUpdate, int value);
-__device__ __host__ void init(int size, char* toFill);
-__device__ __host__ int isFirstBiggerThanSecond(const char* first, const char* second, int size);
-
-
 /*__global__*/ void kernel_add(char* newB, char* first, char* second, int size_first, int size_second, int * size_newB);
 /*__global__ */void kernel_sub(char* newB, const char* first, const char* second, int size_biggest, int diff, int * size_newB);
 /*__global__*/ void kernel_mul(char* newB, const char* first, const char* second, int size_first, int size_second, int * size_newB);
--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
 all : gpu.exe
 
 
-gpu.exe: main.cu BigInteger.o
-	nvcc  $< -o $@ --compiler-options -O2 -arch sm_21 BigInteger.o
+gpu.exe: main.cu BigInteger.o utility.o
+	nvcc  $< -o $@ --compiler-options -O2 BigInteger.o utility.o
 
 BigInteger.o: BigInteger.cu
-		nvcc  $< -c --compiler-options -O2 -arch sm_21
+	nvcc  $< -c --compiler-options -O2
+
+utility.o: utility.cu
+	nvcc $< -c --compiler-options -O2
 
 clean:
+	rm -rf *.o
 	rm -rf *.exe
 
